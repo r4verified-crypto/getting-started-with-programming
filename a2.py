b21e7aa71c453e195c22977c398590c2a266ab8b
r4verified-crypto <r4verified@gmail.com>
2026-05-05 10:24:44 +0530

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,7 @@
+x = 6
+y = 10
+print(x + y)
+codingal = "codingal"
+print(codingal)
+name = input("what is your name: ")
+print("hello",name," how are you?..")
---
+++ a3.py
@@ -0,0 +1,3 @@
+import keyword
+print("the keywords in python are: ")
+print(keyword.kwlist)
